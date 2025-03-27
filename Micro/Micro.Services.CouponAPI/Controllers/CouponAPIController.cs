e60a93ea05556ca838887a2fe25135728dfd5bde
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2025-03-27 21:40:11 +0530

    Common response

---
+++ Micro/Micro.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,54 @@
+﻿using Micro.Services.CouponAPI.Data;
+using Micro.Services.CouponAPI.Models;
+using Micro.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Micro.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> coupons = _db.Coupons.ToList();
+                _response.Result = coupons;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(u => u.CouponId == id);
+                _response.Result = coupon;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+    }
+}
