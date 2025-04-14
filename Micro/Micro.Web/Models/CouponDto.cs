814e214326d53ac30fa7cc2d8411c19b89623e75
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2025-04-14 18:13:20 +0530

    Create Coupon Ui

--- Micro/Micro.Web/Models/CouponDto.cs
+++ Micro/Micro.Web/Models/CouponDto.cs
@@ -1,10 +1,18 @@
-﻿namespace Micro.Web.Models
+﻿using System.ComponentModel;
+
+namespace Micro.Web.Models
 {
     public class CouponDto
     {
         public int CouponId { get; set; }
+
+        [DisplayName("Coupon Code")]
         public string CouponCode { get; set; }
+
+        [DisplayName("Discount Amount")]
         public double DiscountAmount { get; set; }
+
+        [DisplayName("Min Amount")]
         public int MinAmount { get; set; }
     }
 }
