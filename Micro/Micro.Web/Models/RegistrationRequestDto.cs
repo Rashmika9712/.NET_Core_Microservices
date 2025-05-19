1f008176e96e870b556d3cd92ba223cbf15bd734
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2025-05-19 23:00:41 +0530

    Login and Register controller methods

--- Micro/Micro.Web/Models/RegistrationRequestDto.cs
+++ Micro/Micro.Web/Models/RegistrationRequestDto.cs
@@ -1,9 +1,12 @@
-﻿namespace Micro.Web.Models
+﻿using System.ComponentModel;
+
+namespace Micro.Web.Models
 {
     public class RegistrationRequestDto
     {
         public string Email { get; set; }
         public string Name { get; set; }
+        [DisplayName("Phone Number")]
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
         public string? Role { get; set; }
