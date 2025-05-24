a3aa540f8728b36d03a56e1dfdcbfec54852134d
Rashmika Madushan Samarakoon <rashmika@codezync.com>
2025-05-24 20:33:27 +0530

    Token Provider, Update Cookies and authentication

--- Micro/Micro.Web/Utility/SD.cs
+++ Micro/Micro.Web/Utility/SD.cs
@@ -6,6 +6,7 @@
         public static string CouponAPIBase { get; set; }
         public const string RoleAdmin = "ADMIN";
         public const string RoleCustomer = "CUSTOMER";
+        public const string TokenCookie = "JWTToken";
         public enum ApiType
         {
             GET,
